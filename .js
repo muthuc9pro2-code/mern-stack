a3e96e6d0c68e41e172f3da7f999ce59aa8efd41
muthuc9pro2-code <muthuc9pro2@gmail.com>
2026-02-19 18:36:31 +0530

    changed

--- .js
+++ .js
@@ -1,1 +1,28 @@
-console.log("muthu kumar")
+var playgame = confirm("shall we play game");
+if (playgame) {
+    let game = prompt("Choose rock, paper, scissor");
+    if(game) {
+        let player = game.trim().toLowerCase();
+        if(player === "rock" || player === "paper" || player === "scissor") {
+            let computer = Math.floor(Math.random() * 3 + 1);
+            let compresult = computer === 1 ? "rock" 
+            : computer === 2 ? "paper"
+            : "scissor";
+            let result = compresult === player ? "game tie"
+            : compresult === "rock" && player === "scissor" ? `player : ${player}\ncompresult : ${compresult}\ncomputer wins!`
+              : compresult === "paper" && player === "rock" ? `player : ${player}\ncompresult : ${compresult}\ncomputer wins!`
+                : compresult === "scissor" && player === "paper" ? `player : ${player}\ncompresult : ${compresult}\ncomputer wins!`
+                  : `player : ${player}\ncompresult : ${compresult}\nplayer wins!`
+                  alert(result);
+
+        } else {
+            alert("choose rock, paper, scissor")
+        }
+
+    } else{
+        alert("choose something")
+    }
+} else {
+    alert("maybe later")
+}
+
