b28bb22cceece518a8e3dc0bce687e9af95287ac
muthuc9pro2-code <muthuc9pro2@gmail.com>
2026-02-24 13:33:50 +0530

    changed

--- dave.js
+++ dave.js
@@ -1,9 +1,30 @@
-// modules
-import * as se from "./modules.js"
+import {posts} from "./higher function order.js";
 
-console.log(se.default());
-console.log(se.pp());``
-console.log(se.b());
+posts.forEach((post) => {
+    console.log(post);
+});
+console.clear();
+
+
+const filteredpost = posts.filter((post) => {
+    return post.userId === 5;
+})
+console.log(filteredpost);
+
+
+const mappedpost = filteredpost.map((post) => {
+    return post.id * 10;
+})
+console.log(mappedpost);
+
+
+const reducedpostvalue = mappedpost.reduce((sum , post) => {
+    return sum + post;
+});
+console.log(reducedpostvalue)
+
+
+
 
 
 
