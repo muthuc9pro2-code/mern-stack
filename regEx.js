a27abcff27fda0acd4bfb71f5d42be44eb3395a6
muthuc9pro2-code <muthuc9pro2@gmail.com>
2026-03-03 16:23:36 +0530

    changed

--- regEx.js
+++ regEx.js
@@ -1,4 +1,22 @@
-let pattern = /^[\w.]+@[a-zA-Z-]+.[a-zA-Z]{1,3}$/;
-const input = "muthukumar@gmail.com"
-const didmatch = pattern.test(input);
-console.log(didmatch);
+document .getElementById("firstid").addEventListener("input",(event)=> {
+    const regex =/^\(\+91\) ([1-9]\d{3})[- ](\d{6})$/g;
+    const input = document.getElementById("firstid");
+    const value = input.value;
+    const example = document.getElementById("example");
+    const check = regex.test(value);
+    if(!check && value.length) {
+        input.classList.add("inputcolor");
+        example.classList.add("display");
+    } else {
+        input.classList.remove("inputcolor");
+        example.classList.remove("display");
+    }
+}); 
+
+document.getElementById("form").addEventListener("submit",(event)=> {
+    event.preventDefault();
+    const input = document.getElementById("firstid");
+    const regex = /^\(\+91\)[- ]/g 
+    const result = input.value.replaceAll(regex,"");
+    console.log(result);
+});
