156d123caa1d44185b3d109cead8d73fe512a208
muthuc9pro2-code <muthuc9pro2@gmail.com>
2026-03-05 13:49:54 +0530

    changed

--- regEx.js
+++ regEx.js
@@ -1,22 +1,22 @@
-document .getElementById("firstid").addEventListener("input",(event)=> {
-    const regex =/^\(\+91\) ([1-9]\d{3})[- ](\d{6})$/g;
-    const input = document.getElementById("firstid");
-    const value = input.value;
+document.getElementById("inputvalue").addEventListener("input", (event)=> {
+    const input = document.getElementById("inputvalue");
     const example = document.getElementById("example");
+    const value = input.value;
+    const regex = /^(\d{3}-\d{3}-\d{4}|\(\d{3}\)\d{3}-\d{4})$/g;
     const check = regex.test(value);
-    if(!check && value.length) {
-        input.classList.add("inputcolor");
+    if (!check && value.length) {
+        input.classList.add("inputerror");
         example.classList.add("display");
     } else {
-        input.classList.remove("inputcolor");
+         input.classList.remove("inputerror");
         example.classList.remove("display");
     }
-}); 
+});
 
-document.getElementById("form").addEventListener("submit",(event)=> {
+document.getElementById("form").addEventListener("submit", (event)=> {
     event.preventDefault();
-    const input = document.getElementById("firstid");
-    const regex = /^\(\+91\)[- ]/g 
+    const input = document.getElementById("inputvalue");
+    const regex = /[()-]/g;
     const result = input.value.replaceAll(regex,"");
     console.log(result);
-});
+})
